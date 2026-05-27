651f28d92a0382f293663284ef122412dd8ac7be
Gabriel Seow <gbrl.seow123@gmail.com>
2026-05-27 18:22:35 +0800

    test print active bin price

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,16 @@ const mainnetConnection = new Connection('https://api.mainnet-beta.solana.com');
 const USDC_USDT_POOL = new PublicKey('ARwi1S4DaiTG5DX7S4M4ZsrXqpMD1MrTmbu9ue2tpmEq') // You can get your desired pool address from the API https://dlmm-api.meteora.ag/pair/all
 const dlmmPool = await DLMM.create(mainnetConnection, USDC_USDT_POOL);
 
+const activeBin = await dlmmPool.getActiveBin();
+const activeBinPriceLamport = activeBin.price;
+const activeBinPricePerToken = dlmmPool.fromPricePerLamport(
+    Number(activeBin.price)
+);
+
 function main(): void {
     console.log("Hello World!")
+    console.log(activeBinPriceLamport)
+    console.log(activeBinPricePerToken)
 }
 
 main()
