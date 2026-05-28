fe0f76b3502a5f6ee5d43977cd5a70167c4ce98e
Gabriel Seow <gbrl.seow123@gmail.com>
2026-05-28 16:57:15 +0800

    test pool query parameters

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,7 @@ const mainnetConnection = new Connection('https://api.mainnet-beta.solana.com');
 
 const USDC_USDT_POOL = new PublicKey('ARwi1S4DaiTG5DX7S4M4ZsrXqpMD1MrTmbu9ue2tpmEq') // You can get your desired pool address from the API https://dlmm-api.meteora.ag/pair/all
 const dlmmPool = await DLMM.create(mainnetConnection, USDC_USDT_POOL);
-
 const activeBin = await dlmmPool.getActiveBin();
-const activeBinPriceLamport = activeBin.price;
-const activeBinPricePerToken = dlmmPool.fromPricePerLamport(
-    Number(activeBin.price)
-);
 
 async function fetchPoolFees(address: string) {
     const { data, error } = await meteoraApi.GET('/pools/{address}', {
@@ -21,13 +16,17 @@ async function fetchPoolFees(address: string) {
     return data;
 }
 
-function main(): void {
-    console.log("Hello World!")
-    console.log(activeBinPriceLamport)
-    console.log(activeBinPricePerToken)
+async function fetchPools() {
+    const {data, error} = await meteoraApi.GET('/pools', {
+        params: {
+            query: {
+                page_size: Number(10),
+                sort_by: 'fee_tvl_ratio_30m:desc'
+            }},
+    });
+    if (error) throw error;
+    return data;
 }
-
-main()
 
 const pool = await fetchPoolFees(USDC_USDT_POOL.toBase58());
 console.log(`Pool ${pool.name} fees:`);
@@ -36,3 +35,13 @@ console.log(`  1h:   $${pool.fees['1h']}`);
 console.log(`  24h:  $${pool.fees['24h']}`);
 console.log(`  TVL:  $${pool.tvl}`);
 console.log(`  APR:  ${pool.apr}%`);
+
+const pools = await fetchPools();
+for (const pool of pools.data) {
+    console.log(`Pool ${pool.name} fees:`);
+    console.log(`  30m:  $${pool.fees['30m']}`);
+    console.log(`  1h:   $${pool.fees['1h']}`);
+    console.log(`  24h:  $${pool.fees['24h']}`);
+    console.log(`  TVL:  $${pool.tvl}`);
+    console.log(`  APR:  ${pool.apr}%`);
+}
